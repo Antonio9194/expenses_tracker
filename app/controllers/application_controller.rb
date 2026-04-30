2f7ecfb8f1beae543fe7c13bef46340311941d12
Antonio Vinciguerra <antoniovinciguerra@Antonios-Mac-mini.local>
2026-04-30 12:58:06 +0900

    fixed forms ui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [ :currency ])
   end
+
 end
