3609d01a0f50db8e270c0c718b804b6591e051a9
Antonio Vinciguerra <antoniovinciguerra@Antonios-Mac-mini.local>
2026-04-30 12:01:41 +0900

    modified design to be more app like

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,20 @@ module ApplicationHelper
     "CNY" => "¥"
   }.freeze
 
+  CATEGORY_ICONS = {
+    "food"          => "bi-cup-hot",
+    "transport"     => "bi-train-front",
+    "shopping"      => "bi-bag",
+    "health"        => "bi-heart-pulse",
+    "entertainment" => "bi-controller",
+    "housing"       => "bi-house",
+    "other"         => "bi-three-dots"
+  }.freeze
+
+  def category_icon(category)
+    CATEGORY_ICONS[category.to_s] || "bi-three-dots"
+  end
+
   def format_currency(amount)
     symbol = CURRENCY_SYMBOLS[current_user.currency] || current_user.currency
     number_to_currency(amount, unit: symbol, format: "%u %n", precision: 0, delimiter: ".")
